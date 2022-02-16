c2990e607ca7774b6c1e8c5959475982223d1cdf
Taeil-Nam <taeil314@naver.com>
2022-02-16 18:24:14 +0900

    modify

--- 백준/단계/배열/1546 평균.py
+++ 백준/단계/배열/1546 평균.py
@@ -9,7 +9,7 @@ score = list(map(int, input().split()))  # 50, 70
 max_score = max(score)  # 70
 
 for i in range(n):
-    new_score.append(score[i] / max_score * 100)  # 7000 / score[i]
+    new_score.append(score[i] / max_score * 100)
 
 for i in range(n):
     sum = sum + new_score[i]
